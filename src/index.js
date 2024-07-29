797be69e296bf202bbaa4112485b67a72e87a3d3
Ed Hunter <ep.hunter23@gmail.com>
2024-07-29 16:49:38 +0800

    feat: add tab clearing functionality

--- src/index.js
+++ src/index.js
@@ -7,5 +7,27 @@ console.log(insertHome)
 insertHome()
 
 
+
+addTabListeners()
+function addTabListeners(){
+
+    function clearContent(){
+        let contentChildren = document.querySelectorAll('div#content *')
+
+        contentChildren.forEach(node => {
+            node.remove()
+        })
+    }
+
+    let buttons = document.querySelectorAll('nav button')
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () =>{
+            clearContent()
+        }) 
+    });
+}
+
+
 console.log('Hello god')
 //$ npx webpack --watch
